257bc9a493852aab1f1f494fa97df63f2497a9f4
Marvin Alegre <alegremrvn@protonmail.com>
2022-08-04 14:30:18 +0800

    added /api/users/:_id/exercises post handler

--- index.js
+++ index.js
@@ -23,12 +23,17 @@ app.get('/', (req, res) => {
 app.post('/api/users', function (req, res) {
   let data = {
     _id: String(usersData.length),
-    username: req.body.username
+    username: req.body.username,
+    count: 0,
+    log: []
   }
 
   usersData.push(data)
 
-  res.send(data)
+  res.send({
+    _id: data._id,
+    username: data.username
+  })
 })
 
 app.get('/api/users', function (req, res) {
@@ -42,6 +47,59 @@ app.get('/api/users', function (req, res) {
   }
 
   res.send(users)
+})
+
+app.post('/api/users/:_id/exercises', function (req, res) {
+  let count = 0
+  let index
+  for (let i = 0; i < usersData.length; i++) {
+    if (req.params._id != usersData[i]._id) {
+      count++
+    } else {
+      index = i
+    }
+  }
+  if (count === usersData.length) {
+    res.json({
+      error: "No such id exist in the database"
+    })
+  } else if (req.body.date === '' || req.body.date === undefined) {
+    usersData[index].count = usersData[index].count + 1
+
+    let data = {
+      description: req.body.description,
+      duration: parseInt(req.body.duration),
+      date: new Date()
+    }
+
+    usersData[index].log.push(data)
+
+    res.json({
+      _id: usersData[index]._id,
+      username: usersData[index].username,
+      date: data.date.toDateString(),
+      duration: data.duration,
+      description: data.description
+    })
+  } else {
+    usersData[index].count = usersData[index].count + 1
+
+    let data = {
+      description: req.body.description,
+      duration: parseInt(req.body.duration),
+      date: new Date(req.body.date)
+    }
+
+    usersData[index].log.push(data)
+
+    res.json({
+      _id: usersData[index]._id,
+      username: usersData[index].username,
+      date: data.date.toDateString(),
+      duration: data.duration,
+      description: data.description
+    })
+  }
 })
 
 
