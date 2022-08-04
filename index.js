ecff0ca0d72241abadf4d2dd86a30edba3b81caf
Marvin Alegre <alegremrvn@protonmail.com>
2022-08-04 13:24:46 +0800

    added body-parser and /api/users post handler

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
+
+
+
+const usersData = []
+
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.post('/api/users', function (req, res) {
+  res.send({
+    _id: usersData.length,
+    username: req.body.username
+  })
+})
 
 
 
